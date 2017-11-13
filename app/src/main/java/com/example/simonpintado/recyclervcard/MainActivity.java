43b597a072c653b48099bed0a9bbb9d874054e18
Simón Pintado <xgrap1207@gmail.com>
2017-11-12 22:24:12 -0700

    Se agrego la funcion agregar y eliminar, ademas una animacion para dichas acciones

--- app/src/main/java/com/example/simonpintado/recyclervcard/MainActivity.java
+++ app/src/main/java/com/example/simonpintado/recyclervcard/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.simonpintado.recyclervcard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -17,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
+    private int counter=0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +34,40 @@ public class MainActivity extends AppCompatActivity {
         mAdapter=new MyAdapter(names, R.layout.recycler_view_item, new MyAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(String name, int position) {
-                Toast.makeText(MainActivity.this,name+" - "+position, Toast.LENGTH_SHORT).show();
+                //Toast.makeText(MainActivity.this,name+" - "+position, Toast.LENGTH_SHORT).show();
+                deleteName(position);
             }
         });
+
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
 
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.add_name:
+                this.addName(0);
+                return true;
+            default:
+                return  super.onOptionsItemSelected(item);
+        }
+
+
+
+    }
+
+
+
 
     private List<String> getAllNames(){
         return  new ArrayList<String>(){{
@@ -44,5 +76,18 @@ public class MainActivity extends AppCompatActivity {
             add("Mayra");
             add("Luis");
         }};
+    }
+
+    private void addName(int position) {
+        names.add(position,"new Name"+(++counter));
+        mAdapter.notifyItemInserted(position);
+        mLayoutManager.scrollToPosition(position);
+
+    }
+
+    private void deleteName(int position){
+        names.remove(position);
+        mAdapter.notifyItemRemoved(position);
+
     }
 }
